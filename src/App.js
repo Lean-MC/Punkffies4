75d5fabedbaff5bfba99b9412d5167f70f579333
Lean <leanmaciel91@gmail.com>
2022-04-10 20:21:01 -0300

    3ra actualización

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import Counter from './components/Counter/Counter';
 import Navbar from './components/Navbar/Navbar'
 
+
 const App = () => {
- const [show, setShow] = useState(true)
+const [show, setShow] = useState({ path: 'list', param: '1'})
 
   const handleOnAdd = (quantity) => {
     console.log(`se agregaron ${quantity} productos`)
@@ -13,9 +14,11 @@ const App = () => {
 
   return (
       <div className="App">
-        <ItemListContainer greeting={'Hola coders'} />
         <Navbar />
-        
+        <ItemListContainer greeting={'Hola coders'} />
+        <ItemDetailContainer/>
+      
+
         {/* <button onClick={() => setShow(!show)}>{show ? 'Desmontar contador' : 'Montar contador'}</button>
         { show ? <Counter initial={0} stock={10} onAdd={handleOnAdd}/> : null } */}
       </div>
--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,40 +1,47 @@
 const products = [
 
-{
-    id:1,
-    name:'punkffies',
-    price: 1000,
-    category:'food',
-    img: 'https://i.pinimg.com/originals/f2/4f/c3/f24fc332d02af868b8cdd43a2c0d8c96.jpg',
-    stock:20,
-    description:'muffins de bandas de rock'
-},
-{
-    id:1,
-    name:'punkcakes',
-    price: 2000,
-    category:'food',
-    img: 'https://i.pinimg.com/originals/ef/39/fc/ef39fc2052cb72551e9da42e0e645a90.jpg',
-    stock:20,
-    description:'tortas de bandas de rock'
-},
-
-{
-    id:1,
-    name:'punktres',
-    price: 1000,
-    category:'food',
-    img: 'https://i.pinimg.com/564x/95/1d/35/951d35cc62467166f7017cd94b6b17fe.jpg',
-    stock:20,
-    description:'postres rockeros'
-}
-]
-
-export const getProducts = () => {
-    return new Promise (resolve => {
-        setTimeout(() => {
-            resolve(products)
-        }, 2000)
-    })
-} 
- 
+    {
+        id:1,
+        name:'punkffies',
+        price: 1000,
+        category:'food',
+        img: 'https://i.pinimg.com/originals/f2/4f/c3/f24fc332d02af868b8cdd43a2c0d8c96.jpg',
+        stock:20,
+        description:'muffins de bandas de rock'
+    },
+    {
+        id:2,
+        name:'punkcakes',
+        price: 2000,
+        category:'food',
+        img: 'https://i.pinimg.com/originals/ef/39/fc/ef39fc2052cb72551e9da42e0e645a90.jpg',
+        stock:20,
+        description:'tortas de bandas de rock'
+    },
+    
+    {
+        id:3,
+        name:'punktres',
+        price: 1000,
+        category:'food',
+        img: 'https://i.pinimg.com/564x/95/1d/35/951d35cc62467166f7017cd94b6b17fe.jpg',
+        stock:20,
+        description:'postres rockeros'
+    }
+    ]
+    
+    export const getProducts = () => {
+        return new Promise (resolve => {
+            setTimeout(() => {
+                resolve(products)
+            }, 2000)
+        })
+    } 
+    
+    export const getProductsById = (id) => {
+        return new Promise (resolve => {
+            setTimeout(() => {
+                resolve(products.find(prod => prod.id === id))
+            }, 2000)
+        })
+    } 
